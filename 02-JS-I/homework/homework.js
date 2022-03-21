1d7e98def2a4fe5be4e47e57debb9c6d7731b167
HAcosta8 <Her-acosta@hotmail.com>
2022-03-21 20:46:36 -0300

    1 fix

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -41,8 +41,9 @@ function suma(x, y) {
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
-  return y - x;
+  return x - y;
 }
+
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
