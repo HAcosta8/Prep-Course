5325c829e797d858daae8efe49333b2dfdd1db7e
HAcosta8 <Her-acosta@hotmail.com>
2022-03-23 17:32:37 -0300

    final push

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = ("Hernan");
+const nuevaString = "Hernan";
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 8;
@@ -61,20 +61,20 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  If (x == y )
+  if (x == y ) {
    return true;
-  Else
+  }else{
 return false;
-
+  }
 }
 
 function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-   if (str1.length == str2.length); {
+  if (str1.length === str2.lenght) {
     return true;
-     }
+    }
   else {
     return false;
   }
@@ -225,14 +225,14 @@ function obtenerAreaRectangulo(alto, ancho) {
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  
+  return lado * 4;
 }
 
 
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
-
+return (base * altura) / 2 ;
 }
 
 
@@ -240,7 +240,7 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  
+ return euro * 1.20; 
 }
 
 
@@ -250,7 +250,13 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
+  if (letra.length > 1){
+    return "Dato incorrecto";
+  }
+  if (letra === "a"||"e"||"i"||"o"||"u"){
+    return "Es vocal";
+  }
+  return "Dato incorrcto";
 }
 
 
